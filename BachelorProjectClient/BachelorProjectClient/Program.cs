8e037903129523a6ad5269bb394efb1fbbe47c8b
Remes92 <joakimremes@hotmail.com>
2020-02-24 09:31:14 +0100

    Grund för Client

---
+++ BachelorProjectClient/BachelorProjectClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BachelorProjectClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            SoapService.SoapService soap = new SoapService.SoapService();
+            Console.WriteLine(soap.HelloWorld());
+            Console.ReadKey();
+        }
+    }
+}
